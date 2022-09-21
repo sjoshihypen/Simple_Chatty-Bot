537183db8085d517992d05929fc2f22548dc7036
sushantjoshi800 <79572922+sjoshihypen@users.noreply.github.com>
2022-09-21 10:08:05 +0530

    Add files via upload

---
+++ Guess the age.java
@@ -0,0 +1,29 @@
+package bot;
+
+import java.util.Scanner;
+
+public class SimpleBot 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+
+        String name = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int r3 = scanner.nextInt();
+        int r5 = scanner.nextInt();
+        int r7 = scanner.nextInt();
+        int age = (r3 * 70 + r5 * 21 + r7 * 15) % 105;
+
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+    }
+}
+
---
+++ Print your name.java
@@ -0,0 +1,19 @@
+package bot;
+
+import java.util.Scanner;
+
+public class SimpleBot 
+{
+    public static void main(String[] args) 
+   {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+
+        
+        String myName = scanner.next();
+        System.out.println("What a great name you have,Max" + myName);  
+    }
+}
